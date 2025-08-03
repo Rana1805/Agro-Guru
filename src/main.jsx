c0605232cf325cd7a92d5425cf7cefbf854f4968
KAMAL SINGH <Shubhanshu Singh>
2025-08-03 10:56:33 +0530

    Skeleton template

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    
+    <Footer />
   </StrictMode>,
 )
