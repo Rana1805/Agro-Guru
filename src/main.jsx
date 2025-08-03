d6dc6d4422edc83538a4a6bf1ca2253769cf5d78
KAMAL SINGH <Shubhanshu Singh>
2025-08-03 13:11:45 +0530

    checkpoint1

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Home from './Home.jsx'
+import Blog from './Blog.jsx'
 
+
+const [page, setPage] = useState('Home');
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <Header />
     
+
     <Footer />
-  </StrictMode>,
+    
+
+  </StrictMode>
 )
